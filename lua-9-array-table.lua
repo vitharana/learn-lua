195c9a5bce132d29b7766c4471f9cd902205b36f
Sandun Sampath Vitharana <vitharana1996@gmail.com>
2025-05-27 02:24:22 -0500

    added more

--- lua-9-array-table.lua
+++ lua-9-array-table.lua
@@ -1,6 +1,12 @@
 -- array table
 animals = {"cat", "dog", "elephant"}
 
+print(animals[1])
+print(animals[2])
+print(animals[3])
+
+print()
+
 for i = 1, #animals do
     print("Item" ..i.. ":", animals[i])
 end
