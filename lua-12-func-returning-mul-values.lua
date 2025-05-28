e4e6cdf7dec20f4af74aac510b739b68e30b4878
Sandun Sampath Vitharana <vitharana1996@gmail.com>
2025-05-27 22:43:22 -0500

    add file to show anonomous functions and multi return

---
+++ lua-12-func-returning-mul-values.lua
@@ -0,0 +1,18 @@
+function getPosition()
+    return 100, 100
+end
+
+x, y = getPosition()
+
+print("x="..x.." y="..y)
+
+
+boost = function(x)
+    return x * 10
+end
+
+
+-- gives the same result
+print(boost(5))
+
+print(boost("5"))
